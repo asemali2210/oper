fa901587a2085c59854005935d63326f20a940f8
Asem Ali <57010155+asemali2210@users.noreply.github.com>
2023-02-11 13:55:48 +0200

    update | dark Mood & AR language

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var navSm = document.getElementById("navSm");
 var toggleBtn = document.getElementById("toggleBtn");
+var toggleApperance = document.getElementById("toggleApperance");
 
 
 var operHeader = document.getElementById("operHeader");
@@ -10,6 +11,7 @@ var operFqa = document.getElementById("operFqa");
 var operPromotion = document.getElementById("operPromotion");
 
 
+/* left Column items declaration */
 
 var sideHeading = document.getElementById("sideHeading");
 var sideHeaerLogo = document.getElementById("sideHeaerLogo");
@@ -28,15 +30,32 @@ var sideHelpBox = document.getElementById("sideHelpBox");
 var sidePromotion = document.getElementById("sidePromotion");
 var sidePromotionBox = document.getElementById("sidePromotionBox");
 
+/* according declaration */
 var accordingQItems = document.querySelectorAll(".according__q")
 var accordingQList = Array.from(accordingQItems);
 var accordingAItems = document.querySelectorAll(".according__a")
 var accordingAList = Array.from(accordingAItems);
 
+
+
 /* toggle Navbar */
 
 toggleBtn.addEventListener('click', function(){
     navSm.classList.contains('toggle--nav') ? navSm.classList.remove('toggle--nav') : navSm.classList.add('toggle--nav');
+});
+/* toggle apperance */
+
+toggleApperance.addEventListener('click', function(){
+    var body = document.querySelector('body');
+    console.log(body)
+    if(body.classList.contains('light-mood')) {
+        body.classList.remove('light-mood');
+        body.classList.add('dark-mood');
+    } else {
+        body.classList.remove('dark-mood');
+        body.classList.add('light-mood');
+    }
+    
 });
 
 /* toggle Acordding */
@@ -50,27 +69,39 @@ accordingQList.forEach((accoQ) => {
     })
 })
 
+
+
+
 sideHeading.style.height = `${operHeader.getBoundingClientRect().height}px`;
 sideHeaerLogo.style.top = `${operNav.getBoundingClientRect().top + 15}px`;
 sideHeaderBox.style.top = `70%`;
 
+/*
+    ## set posstion to all items of left column dynamic
+    - get section height from the right column 
+    - Set the height of the leftSection that holding the box
+    - set style (top,bottom) to child as we need
+*/
 
-sideAbout.style.height = `${operAbout.getBoundingClientRect().height}px`;
-sideAboutBox.style.bottom = `10%`;
+
+function setItemToLeftColumn(sectionHeight,leftSection,boxOfChild, position){   
+    leftSection.style.height = `${sectionHeight.getBoundingClientRect().height}px`;
+    boxOfChild.style.bottom = `${position}%`;
+    
+}
 
 
-sideAdvantages.style.height = `${operAdvantages.getBoundingClientRect().height}px`;
-sideAdvantagesBox.style.bottom = `15%`;
+setItemToLeftColumn(operAbout,sideAbout,sideAboutBox, 10)
+setItemToLeftColumn(operAdvantages,sideAdvantages,sideAdvantagesBox, 15)
+setItemToLeftColumn(operFqa,sideHelp,sideHelpBox, 20)
+setItemToLeftColumn(operPromotion,sidePromotion,sidePromotionBox, 15)
 
+/* sideAdvantages.style.height = `${operAdvantages.getBoundingClientRect().height}px`;
+sideAdvantagesBox.style.bottom = `15%`; 
 
 sideHelp.style.height = `${operFqa.getBoundingClientRect().height}px`;
-sideHelpBox.style.bottom = `20%`;
-
+sideHelpBox.style.bottom = `20%`; 
 
 sidePromotion.style.height = `${operPromotion.getBoundingClientRect().height}px`;
-sidePromotionBox.style.bottom = `15%`;
+sidePromotionBox.style.bottom = `15%`; */
 
-document.getElementByTagName("body").style.backgroundColor = "#ffff";
-
-console.log(operHeader.getBoundingClientRect())
-console.log(sideHeading.getBoundingClientRect())
